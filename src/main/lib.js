46de92028d9e1899d1d774529c80dd0c4394cc3b
Mikael Lindqvist <li.mikael@gmail.com>
2024-09-24 10:21:26 +0800

    export WhiskerEdSelection

--- src/main/lib.js
+++ src/main/lib.js
@@ -1,5 +1,6 @@
 export {txmlParse, txmlStringify} from "../utils/txml-stringify.js";
 export {default as DocTree} from "../doctree/DocTree.jsx";
 export {default as WhiskerEd} from "../whiskered/WhiskerEd.jsx";
+export {default as WhiskerEdSelection} from "../whiskered/WhiskerEdSelection.js";
 export * from "../utils/xml-util.js";
 export * from "../whiskered/whiskered-util.js";
